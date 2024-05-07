7df49447fde106f081febba90e6c950b9bfe26ca
Nahuel Palacio <palacionahuel202@gmail.com>
2024-05-07 16:15:03 -0300

    1
    1

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import ar.edu.unlam.mobile.scaffolding.ui.components.BottomBar
 import ar.edu.unlam.mobile.scaffolding.ui.screens.HomeScreen
 import ar.edu.unlam.mobile.scaffolding.ui.theme.ScaffoldingV2Theme
 import dagger.hilt.android.AndroidEntryPoint
@@ -51,7 +50,6 @@ fun MainScreen() {
     // a través del back stack
     val controller = rememberNavController()
     Scaffold(
-        bottomBar = { BottomBar(controller = controller) },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = { controller.navigate("home") },
