4e4b23a6c035c665d106fb7b8f8a61f518cb4c52
FacundoTierno <tiernofacundo3@gmail.com>
2024-05-22 17:57:32 -0300

    Creacion de la pantalla Home, y modificacion del FloatingButton

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,11 @@ android {
 }
 
 dependencies {
+    // Room
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
 
     // Base
     implementation(libs.androidx.core.ktx)
