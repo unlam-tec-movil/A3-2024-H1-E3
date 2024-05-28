8665f429d1f0a78e258fe6b0c842f0e1fc126df3
FacundoTierno <tiernofacundo3@gmail.com>
2024-05-28 17:05:10 -0300

    Creacion de la pantalla Home, y modificacion del FloatingButton

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/HomeScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/HomeScreen.kt
@@ -7,41 +7,51 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ar.edu.unlam.mobile.scaffolding.data.local.producto.entity.Producto
 import ar.edu.unlam.mobile.scaffolding.ui.components.usuario.viewmodel.ProductoViewModel
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -50,41 +60,67 @@ fun HomeScreen(
     viewModelP: ProductoViewModel,
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
+
     val productos by viewModelP.productos.collectAsState()
     val uiState: HomeUIState by viewModel.uiState.collectAsState()
     var searchText by remember { mutableStateOf("") }
+
     when (val helloState = uiState.helloMessageState) {
         is HelloMessageUIState.Loading -> {
             // Loading
         }
 
         is HelloMessageUIState.Success -> {
-            Scaffold(
-                topBar = { TopBar() },
-                content = { paddingValues ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(paddingValues),
-                    ) {
-                        BarraDeBusqueda(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp),
-                            searchText = searchText,
-                            onSearchTextChange = { newText ->
-                                searchText = newText
-                            },
-                        )
-                        // Filtrar productos según el texto de búsqueda
-                        val filteredProducts = productos.filter { producto ->
-                            producto.nombre.contains(searchText, ignoreCase = true)
-                        }
-                        Contenido(filteredProducts, onProductoClick = { producto ->
-                            // Maneja el clic del producto aquí
-                            println("Producto clickeado: ${producto.nombre}")
-                        })
-                    }
+            ModalNavigationDrawer(
+                drawerState = drawerState,
+                drawerContent = {
+                    DrawerContent(
+                        onMenuItemClick = {
+                            coroutineScope.launch {
+                                drawerState.close()
+                            }
+                        },
+                    )
+                },
+                content = {
+                    Scaffold(
+                        topBar = {
+                            TopBar(
+                                onMenuClick = {
+                                    coroutineScope.launch {
+                                        drawerState.open()
+                                    }
+                                },
+                            )
+                        },
+                        content = { paddingValues ->
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(paddingValues),
+                            ) {
+                                BarraDeBusqueda(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp),
+                                    searchText = searchText,
+                                    onSearchTextChange = { newText ->
+                                        searchText = newText
+                                    },
+                                )
+                                // Filtrar productos según el texto de búsqueda
+                                val filteredProducts = productos.filter { producto ->
+                                    producto.nombre.contains(searchText, ignoreCase = true)
+                                }
+                                Contenido(filteredProducts, onProductoClick = { producto ->
+                                    // Maneja el clic del producto aquí
+                                    println("Producto clickeado: ${producto.nombre}")
+                                })
+                            }
+                        },
+                    )
                 },
             )
         }
@@ -127,24 +163,6 @@ fun Contenido(productoEntities: List<Producto>, onProductoClick: (Producto) -> U
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TopBar() {
-    TopAppBar(
-        title = { Text(text = "") },
-        actions = {
-            IconButton(onClick = { /* Acción al hacer clic */ }) {
-                Icon(Icons.Filled.Share, contentDescription = "Share")
-            }
-        },
-        navigationIcon = {
-            IconButton(onClick = { /* Acción al hacer clic */ }) {
-                Icon(Icons.Filled.MoreVert, contentDescription = "Menu")
-            }
-        },
-    )
-}
-
 @Composable
 fun BarraDeBusqueda(
     modifier: Modifier = Modifier,
@@ -181,6 +199,57 @@ fun ProductoItem(producto: Producto, onClick: () -> Unit) {
             Text(text = "Precio: ${producto.precio}")
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = "Stock: ${producto.stock}")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(onMenuClick: () -> Unit) {
+    TopAppBar(
+        title = { Text(text = "") },
+        actions = {
+            IconButton(onClick = { /* Acción al hacer clic */ }) {
+                Icon(Icons.Filled.Share, contentDescription = "Share")
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = onMenuClick) {
+                Icon(Icons.Filled.Menu, contentDescription = "Menu")
+            }
+        },
+    )
+}
+
+@Composable
+fun DrawerContent(onMenuItemClick: () -> Unit) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier
+            .fillMaxHeight()
+            .width(300.dp), // Asegura que el drawer ocupe solo la mitad izquierda
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "Tienda",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp),
+            )
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+
+            val menuItems = listOf("Inicio", "Agragar stock", "vender", "Balance", "Configuracion")
+
+            menuItems.forEach { menuItem ->
+                Text(
+                    text = menuItem,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                        .clickable(onClick = onMenuItemClick),
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+            }
         }
     }
 }
