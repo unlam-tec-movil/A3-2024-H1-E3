8fe64a72e2f75df6ab703e04cc7bbbfc64fd097d
Nahuel Palacio <palacionahuel202@gmail.com>
2024-06-17 10:52:12 -0300

    1
    1

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/crear_producto/CrearProducto.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/crear_producto/CrearProducto.kt
@@ -21,21 +21,42 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import ar.edu.unlam.mobile.scaffolding.data.local.database.InventoryDatabase
+import ar.edu.unlam.mobile.scaffolding.data.repository.producto.OfflineProductoRepository
+import ar.edu.unlam.mobile.scaffolding.ui.components.MyTopBar
 import ar.edu.unlam.mobile.scaffolding.ui.components.producto.viewmodel.ProductoViewModel
 import kotlinx.coroutines.launch
+
+@Preview
+@Composable
+fun CrearProductoPreview() {
+    val context = LocalContext.current
+    val db = Room.databaseBuilder(context, InventoryDatabase::class.java, "producto_db").build()
+    val dao = db.producotDao()
+    val repository = OfflineProductoRepository(dao)
+    val viewModel = ProductoViewModel(repository)
+    val navController: NavHostController = rememberNavController()
+
+    CrearProducto(navController, viewModel)
+}
 
 @Composable
 fun CrearProducto(
@@ -43,6 +64,10 @@ fun CrearProducto(
     viewModel: ProductoViewModel,
 ) {
     val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = { MyTopBar(onNavigateBack = { /*TODO*/ }, title = a) },
+    ) {
+    }
     Column(
         modifier =
             Modifier
