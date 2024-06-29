d06eef98dddbc04f820f89c5b2f3773b3c9b6716
Nahuel Palacio <palacionahuel202@gmail.com>
2024-06-29 13:40:37 -0300

    1
    1

--- app/src/test/java/ar/edu/unlam/mobile/scaffolding/ProductoViewModelTest.kt
+++ app/src/test/java/ar/edu/unlam/mobile/scaffolding/ProductoViewModelTest.kt
@@ -1,5 +1,4 @@
 package ar.edu.unlam.mobile.scaffolding
-import android.content.Context
 import ar.edu.unlam.mobile.scaffolding.data.local.producto.dao.ProductoDao
 import ar.edu.unlam.mobile.scaffolding.data.repository.producto.OfflineProductoRepository
 import ar.edu.unlam.mobile.scaffolding.domain.usecases.productos.GuardarProductoUseCase
@@ -9,7 +8,6 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
 import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
 
@@ -34,14 +32,10 @@ class ProductoViewModelTest {
             viewModel.ubicacionProveedor = LatLng(100.15, -100.12)
             viewModel.qr = "QR123"
 
-            val context = Mockito.mock(Context::class.java)
-
             // Llamar al método bajo prueba
             val result = viewModel.guardarProducto()
 
             // Verificar que el producto se haya guardado correctamente
             assertEquals("", viewModel.nombre) // Verificar que los campos se limpien después de guardar
         }
-
-    // Puedes agregar más tests para otros métodos del ViewModel aquí
 }
